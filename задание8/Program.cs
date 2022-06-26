96ff2f8472b9f24d180caeae7c9fdd9dc99245bf
Marina <gtellier.mecanics@gmail.com>
2022-06-26 14:37:59 +0300

    Программа, которая показывает все четные числа

---
+++ задание8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int even = 2;
+while(even <= number)
+{
+    if(even % 2 == 0)
+    {
+        Console.Write($"{even}, ");
+        even++;
+    }
+    else
+    {
+        even++;
+    }
+}
